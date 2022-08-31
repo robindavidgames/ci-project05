3c079a75689b66a9e704a52c56fd81d5118c1bee
Robin David <robin.david.games@gmail.com>
2022-08-31 17:00:24 +0000

    Add basic product_view page.

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name="products")
-    path('product_id', views.product_view, name="product_view")
+    path('', views.all_products, name="products"),
+    path('<product_id>', views.product_view, name="product_view"),
 ]
