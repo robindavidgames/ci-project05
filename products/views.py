23fc9b6fd0f741ac301879435d9ee39da1b1ade3
Robin David <robin.david.games@gmail.com>
2022-08-31 19:32:40 +0000

    Add filter by category.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 # Modified from Boutique Ado project.
@@ -10,22 +10,29 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-                # Using Q to allow search in title OR description.
-                # i before 'contains' makes the seach case insensitive
+            # Using Q to allow search in title OR description.
+            # i before 'contains' makes the seach case insensitive
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
