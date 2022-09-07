fb70a61047c2cb770854f99a776c1aa4d20fb59c
Robin David <robin.david.games@gmail.com>
2022-09-07 14:44:58 +0000

    Display bag items in bag.

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,7 +21,7 @@ def bag_contents(request):
             'product': product,
         })
 
-    if total<settings.FREE_DELIVERY_THRESHOLD:
+    if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
