700d23eea8e093a3567317ed7b08dfba8b83b47f
Robin David <robin.david.games@gmail.com>
2022-09-09 12:30:40 +0000

    Add adjust_bag view and URL.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 def view_bag(request):
@@ -6,7 +6,7 @@ def view_bag(request):
 
     return render(request, 'bag/shopping_bag.html')
 
-# Modified from Boutique Ado project.
+# Heavily modified from Boutique Ado project.
 def add_to_bag(request, item_id):
     """ Add quantity of an item to the shopping bag. """
 
@@ -40,8 +40,8 @@ def add_to_bag(request, item_id):
     
     # Check if item has a variant
     if 'variant' in request.POST:
-        print(request.POST)
         variant = request.POST.get('variant')
+
         # If it has a variant, check if there is already one of that style.
         # If not, add a new item. If so, add one to existing item.
         if item_id in list(bag.keys()):
@@ -51,6 +51,7 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_variant'][variant] = quantity
         else:
             bag[item_id] = {'items_by_variant': {variant: quantity}}
+
     # If item doesn't have a variant, create a standard entry.
     # Update item entry, if it is already in the bag.
     else:
@@ -60,5 +61,28 @@ def add_to_bag(request, item_id):
             bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust quantity of an item to the shopping bag. """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+    
+    # Check if item has a variant
+    if 'variant' in request.POST:
+        variant = request.POST.get('variant')
+        if quantity > 0:
+            bag[item_id]['items_by_variant'][variant] = quantity
+        else:
+            del bag[item_id]['items_by_variant'][variant]
+
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
