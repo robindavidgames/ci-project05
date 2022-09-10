9e5c7bfc5933b27a11d9662c82ab82166b52021d
Robin David <robin.david.games@gmail.com>
2022-09-10 06:55:26 +0000

    Clean up bag/views.py.

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 
-# Create your views here.
 def view_bag(request):
     """ A view to return the shopping_bag page. """
 
@@ -12,31 +11,7 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    # product_variant = None
-
-    # if 'variant' in request.POST:
-    #     product_variant = request.POST('variant')
-
     bag = request.session.get('bag', {})
-
-    # if product_variant:
-    #     if item_id in list(bag.keys()):
-    #         if variant in bag[item_id]['items_by_variant'].keys():
-    #             bag[item_id]['items_by_variant'][variant] += quantity
-    #         else:
-    #             bag[item_id]['items_by_variant'][variant] = quantity
-    #     else:
-    #         bag[item_id] = {'items_by_variant': {variant: quantity}}
-
-    # if 'variant' in request.POST:
-        # if item_id in list(bag.keys()):
-        #     if variant in bag[item_id]['items_by_variant'].keys():
-        #         bag[item_id]['items_by_variant'][variant] += quantity
-        #     else:
-        #         bag[item_id]['items_by_variant'][variant] = quantity
-        # else:
-        # product_variant = request.POST('variant')
-        # bag[item_id] = {'items_by_variant': {product_variant: quantity}}
     
     # Check if item has a variant
     if 'variant' in request.POST:
@@ -69,7 +44,7 @@ def adjust_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
-    
+
     # Check if item has a variant
     if 'variant' in request.POST:
         variant = request.POST.get('variant')
