94b9807a8881a5d3f20437fa6b36cbe43805f0f4
Robin David <robin.david.games@gmail.com>
2022-10-02 15:45:45 +0000

    Create checkout template, views. Install crispy forms.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+# Modified from Boutique Ado sample project
+def checkout(request):
+    bag = request.session.get('bag', {})
+    # To prevent a user typing the checkout URL.
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
