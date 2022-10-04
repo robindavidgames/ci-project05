10bd4b180ff76a0f71c387c9c2756a61d4846fa7
Robin David <robin.david.games@gmail.com>
2022-10-04 15:51:16 +0000

    Add checkout success view and URL.

--- checkout/views.py
+++ checkout/views.py
@@ -36,21 +36,12 @@ def checkout(request):
                 try:
                     product = Product.objects.get(id=item_id)
 
-                    if product.has_variants:
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                            variant=product_variant,
-                        )
-                        order_line_item.save()
-                    else:
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
 
                     # If integer, then doesn't have variants.
                     # if isinstance(item_data, int):
@@ -111,6 +102,27 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
