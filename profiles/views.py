556a7575dfd6470f1d5e8998277ee7ec790131b5
Robin David <robin.david.games@gmail.com>
2022-10-13 09:23:40 +0000

    Add update information form to user profile.

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Modified from Boutique Ado sample project
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
+    # Check if profile context is used in template and delete context if not.
     context = {
+        'form': form,
+        'orders': orders,
         'profile': profile,
     }
 
