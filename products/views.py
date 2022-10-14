b6d56c50e49187c104d2327e6333048208aeef69
Robin David <robin.david.games@gmail.com>
2022-10-14 11:17:01 +0000

    Correct typo in product edit view.

--- products/views.py
+++ products/views.py
@@ -100,7 +100,7 @@ def edit_product(request, product_id):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('product_view', args=[product.id]))
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
