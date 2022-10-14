e54890b2a84be0ca6f1656eee3689a8cffc8c54f
Robin David <robin.david.games@gmail.com>
2022-10-14 20:43:50 +0000

    Add custom clearable file input widget.

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 # Modified from Boutique Ado sample project.
@@ -9,6 +10,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
