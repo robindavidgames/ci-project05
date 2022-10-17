469201d988e93432d3d31c115d6fdd9d00ab7333
Robin David <robin.david.games@gmail.com>
2022-10-17 18:45:24 +0000

    Update index with gallery view.

--- home/views.py
+++ home/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
 from products.models import Product
 
-# def index(request):
-#     """ A view to return the index page. """
-
-#     return render(request, 'home/index.html')
 
 def index(request):
     """ A view to return the index page. """
-    # products = Product.objects.all()
-    # best_selling = products.filter(category__name__in='best-selling')
-    # best_selling = products.filter(category__name__in='best-selling')
-
-    # Entry.objects.all().filter(pub_date__year=2006)
-    # Entry.objects.filter(pub_date__year=2006)
-
-    best_selling = Product.objects.all().filter(category__name='best-selling')
-
+    # Square bracket limits how many objects are returned.
+    best_selling = Product.objects.all().filter(category__name='best-selling')[:4]
+    cooking = Product.objects.all().filter(category__name='cooking')[:4]
     context = {
         'best_selling': best_selling,
+        'cooking': cooking,
     }
 
     return render(request, 'home/index.html', context)
 
-# if 'category' in request.GET:
-#             categories = request.GET['category'].split(',')
-#             products = products.filter(category__name__in=categories)
-#             categories = Category.objects.filter(name__in=categories)
